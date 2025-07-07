4599e49585841f2db7acde38e10033e2833dca71
harshav5440 <harshachandu016@gmail.com>
2025-07-07 21:33:10 +0530

    javafundamentals

---
+++ java_fundamentals/src/java_fundamentals/Question11.java
@@ -0,0 +1,36 @@
+package java_fundamentals;
+import java.util.*;
+public class Question11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+	        char code = scanner.next().toUpperCase().charAt(0);
+	        switch (code) {
+	            case 'R':
+	                System.out.println("Color Name: Red");
+	                break;
+	            case 'B':
+	                System.out.println("Color Name: Blue");
+	                break;
+	            case 'G':
+	                System.out.println("Color Name: Green");
+	                break;
+	            case 'O':
+	                System.out.println("Color Name: Orange");
+	                break;
+	            case 'Y':
+	                System.out.println("Color Name: Yellow");
+	                break;
+	            case 'W':
+	                System.out.println("Color Name: White");
+	                break;
+	            default:
+	                System.out.println("Invalid Code");
+	        }
+
+	        scanner.close();
+	    }
+	}
+
+
---
+++ java_fundamentals/src/java_fundamentals/Question16.java
@@ -0,0 +1,30 @@
+package java_fundamentals;
+import java.util.*;
+public class Question16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		boolean isPrime=true;
+		if(n <= 1) {
+			isPrime=false;
+		}
+		else {
+			for(int i=2 ;i * i<=n;i++) {
+				if( n % i == 0) {
+					isPrime=false;
+				}
+			}
+		}
+		if(isPrime) {
+			System.out.println("prime"+n);
+		}
+		else {
+			System.out.println("not prime");
+		}
+
+
+	}
+
+}
